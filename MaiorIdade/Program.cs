69d007184a09461683358c67341c567c9f43afa3
gutsserrano <gutsserrano@gmail.com>
2024-04-18 21:40:31 -0300

    Add project files.

---
+++ MaiorIdade/Program.cs
@@ -0,0 +1,25 @@
+﻿// Faça um algorítmo que leia a idade de uma pessoa e informe se ela é maior ou menor de idade
+
+int idade;
+
+Console.WriteLine("Digite sua idade: ");
+idade = int.Parse(Console.ReadLine());
+
+if (idade < 0)
+{
+    Console.WriteLine("Idade inválida!\n");
+}
+else
+{
+    if (idade < 18)
+    {
+        Console.WriteLine("Menor de idade\n");
+    }
+    else
+    {
+        Console.WriteLine("Maior de idade\n");
+    }
+}
+
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.ReadKey();
